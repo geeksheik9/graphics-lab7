a2a1a9db0251dd6f45fe614ecc22ca0505ae2ca9
Abrams, Benjamin <blabrams@ilstu.edu>
2021-10-20 16:06:50 -0400

    added depth fields, lab working

--- one_square.js
+++ one_square.js
@@ -41,28 +41,24 @@ var vertexColors = [
 	[ 0.0, 1.0, 1.0, 1.0 ] // cyan
 	];
 
-var gIndices = [
-	1,0,3,
-	3,2,1,
-	2,3,7,
-	7,6,2,
-	3,0,4,
-	4,7,3,
-	6,5,1,
-	1,2,6,
-	4,5,6,
-	6,7,4,
-	5,4,0,
-	0,1,5
-];
+var indices = [
+	1, 0, 3,
+	3, 2, 1,
+	2, 3, 7,
+	7, 6, 2,
+	3, 0, 4,
+	4, 7, 3,
+	6, 5, 1,
+	1, 2, 6,
+	4, 5, 6,
+	6, 7, 4,
+	5, 4, 0,
+	0, 1, 5
+	];
 
 var faces = new Array(24);
 
 var numVertices = 36;
-var points = [];
-var colors = [];
-
-var colors = [];
 
 var offset = 0;
 
@@ -107,96 +103,42 @@ window.onload = function init() {
 
 	colorLoc = gl.getUniformLocation(program, "vertColor");
 
+	iBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
 	vBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, (64*30000), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 	vPosition = gl.getAttribLocation(program, "vPosition");
 	gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vPosition);
 
 	colorBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, (96*30000), gl.STATIC_DRAW)
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW)
 	var vColor = gl.getAttribLocation(program, "vColor");
 	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0 , 0);
 	gl.enableVertexAttribArray(vColor)
 
-	iBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, (96*30000), gl.STATIC_DRAW);
-
-	colorCube();
+	gl.clearDepth(1.0);
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
 
     render();
 };
-
-function colorCube() {
-	quad(1,0,3,2);
-	quad(2,3,7,6);
-	quad(3,0,4,7);
-	quad(6,5,1,2);
-	quad(4,5,6,7);
-	quad(5,4,0,1);
-}
-
-function quad(a,b,c,d) {
-	var indices = [a,b,c,a,c,d];
-
-	for(let i = 0; i < indices.length; i++){
-		points.push(vertices[indices[i]]);
-		colors.push(vertexColors[indices[i]]);
-	}
-} 
-
-function rotateX(theta){
-	rotation = mat4(
-		1,0,0,0,
-		0,Math.cos(theta),-Math.sin(theta),0,
-		0,Math.sin(theta),Math.cos(theta),0,
-		0,0,0,1, 
-	)
-	return rotation
-}
-
-function rotateY(theta){
-	rotation = mat4(
-		Math.cos(theta), 0, Math.sin(theta), 0,
-		0,1,0,0,
-		-Math.sin(theta),0, Math.cos(theta), 0,
-		0,0,0,1
-	)
-	return rotation
-}
-
-function rotateZ(theta){
-	rotation = math4(
-		Math.cos(theta), -Math.sin(theta),0,0,
-		Math.sin(theta), Math.cos(theta),0,0,
-		0,0,1,0,
-		0,0,0,1
-	)
-	return rotation
-}
 
 function render() {
 	// this is render loop
 
 	// clear the display with the background color
-    gl.clear( gl.COLOR_BUFFER_BIT);
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	if(rotating){
 		theta[axis] += 2;
 	}
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
-	
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
-
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(gIndices), gl.STATIC_DRAW)
-
+	gl.uniform3fv(thetaLoc, theta);
 	gl.drawElements(gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0)
 
     setTimeout(
@@ -208,20 +150,21 @@ function setRotate(input){
 	switch (input) {
 		case 'X':
 			rotating = true;
-			console.log("rotating x")
 			axis = xAxis;
 			break;
 
 		case 'Y':
 			rotating = true;
-			console.log("rotating y")
 			axis = yAxis;
 			break;
 
 		case 'Z':
 			rotating = true;
-			console.log("rotating z")
 			axis = zAxis;
+			break;
+
+		case 'stop':
+			rotating = false;
 			break;
 
 		default:
